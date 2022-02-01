2e885e8247214a443599712a9ddaa5a9aa44f1ad
mostfa-sadoon <54408018+mostfa-sadoon@users.noreply.github.com>
2022-02-01 17:01:01 +0200

    video

--- resources/views/admin/idea/show.blade.php
+++ resources/views/admin/idea/show.blade.php
@@ -50,8 +50,10 @@
           src="{{$idea->video}}">
     </iframe>
 
+    
 
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/ETVAhPIkebc?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="560" height="315" src="{{$idea->video}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 
         <iframe width="560" height="315" src="https://www.youtube.com/embed/EIfsXEtEUhQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
