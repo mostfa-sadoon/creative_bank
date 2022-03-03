b2cda255d92222ab231230bb6f0703659e2d3503
NesmaElsafty <nesmaelsafty18@gmail.com>
2022-03-03 13:37:17 +0200

    ContactInfo_API

--- app/Http/Controllers/Api/InfoController.php
+++ app/Http/Controllers/Api/InfoController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\About;
+use App\Models\ContactInfo;
 use App\Http\Resources\AboutResource;
+use App\Http\Resources\ContactInfoResource;
 
 class InfoController extends Controller
 {
@@ -14,7 +16,14 @@ class InfoController extends Controller
     {     
             $data = About::latest()->get(); 
             return msgdata(true,'retuen success',$data);
-     }
+    }
+
+    public function ContactInfo_Index()
+    {     
+            $data = ContactInfo::latest()->get(); 
+            // dd($data)
+            return msgdata(true,'retuen success',$data);
+    }
 
 
 }
