8ef6a4b3cf90bc16d682391d30d3b1ee723e990c
Mostafa Saadoun <54408018+mostfa-sadoon@users.noreply.github.com>
2022-03-07 13:13:45 +0200

    edite_news

--- resources/views/admin/new/edit.blade.php
+++ resources/views/admin/new/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('admin_temp')
 @section('styles')
-<link href="{{asset('../assets/plugins/summernote/dist/summernote.css')}}" rel="stylesheet" />
-<link rel="stylesheet" href="{{asset('../assets/plugins/dropify/dist/css/dropify.min.css')}}">
+<link href="{{asset('assets/plugins/summernote/dist/summernote.css')}}" rel="stylesheet" />
+<link rel="stylesheet" href="{{asset('assets/plugins/dropify/dist/css/dropify.min.css')}}">
 @endsection
 @section('content')
             <!-- Bread crumb and right sidebar toggle -->
@@ -140,18 +140,18 @@
     <!-- ============================================================== -->
     <!-- Style switcher -->
     <!-- ============================================================== -->
-    <script src="{{asset('../assets/plugins/styleswitcher/jQuery.style.switcher.js')}}"></script>
+    <script src="{{asset('assets/plugins/styleswitcher/jQuery.style.switcher.js')}}"></script>
     <script src="{{asset('js/sidebarmenu.js')}}"></script>
     <!--stickey kit -->
-    <script src="{{asset('../assets/plugins/sticky-kit-master/dist/sticky-kit.min.js')}}"></script>
-    <script src="{{asset('../assets/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/sticky-kit-master/dist/sticky-kit.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
     <!--Custom JavaScript -->
     <script src="{{asset('js/custom.min.js')}}"></script>
     <!-- ============================================================== -->
     <!-- Plugins for this page -->
     <!-- ============================================================== -->
     <!-- jQuery file upload -->
-    <script src="{{asset('../assets/plugins/dropify/dist/js/dropify.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/dropify/dist/js/dropify.min.js')}}"></script>
     <script>
     $(document).ready(function() {
         // Basic
